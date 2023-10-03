e4d6a06f8a2a8b1515a47f90ae71640d7fcf8b54
sers1 <sers156@mail.ru>
2023-10-03 20:53:44 +1000

    initial commit

---
+++ src/Homework/Exercise2.java
@@ -0,0 +1,7 @@
+package Homework;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Homework/Exercise4.java
@@ -0,0 +1,7 @@
+package Homework;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+
+    }
+}
