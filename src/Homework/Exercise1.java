2dc3143b6e7022ba6efedc7514448eaa78deb5ba
sers1 <sers156@mail.ru>
2023-10-03 21:13:44 +1000

    hw4

--- src/Homework/Exercise1.java
+++ src/Homework/Exercise1.java
@@ -2,6 +2,29 @@ package Homework;
 
 public class Exercise1 {
     public static void main(String[] args) {
+        System.out.println("Part 3.2.1");
+        System.out.println(" >,<,>=,<=,==,!=");
+
+        System.out.println("Part 3.2.2");
+
+        int x=1;
+        boolean b1= x>0;
+        System.out.println(b1);
+        boolean b2= x<0;
+        System.out.println(b2);
+        boolean b3= x!=0;
+        System.out.println(b3);
+        boolean b4= x>=0;
+        System.out.println(b4);
+        boolean b5= x!=1;
+        System.out.println(b5);
+        System.out.println("Part 3.2.3");
+
+        boolean b = true;
+        int i = (boolean) b;
+        int i =1;
+        boolean b = (int)i;
+        // тип данных boolean не может быть переделан в другой тип данных
 
     }
 }
